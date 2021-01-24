e83dfa37e2d5d7ebb45e99b822a1616a50d1e97b
Hunk <364125707@qq.com>
2021-01-24 09:32:43 +0800

    登陆

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="login">
     <van-loading type="spinner" vertical color="#1989fa" size="40">
-      正在登录中 ...</van-loading
-    >
+      正在登录中 ...
+    </van-loading>
   </div>
 </template>
 <script>
 import { Loading } from "vant";
 import { loginApi } from "@/api/login.js";
+import { wxclientloginApi } from "@/api/index";
+
 export default {
   components: {
     [Loading.name]: Loading,
@@ -23,11 +25,59 @@ export default {
   },
   methods: {
     async executeLogin() {
-      let res = await loginApi();
-      if (res.ack == "200") {
-        this.$store.commit("setAuthToken", res.data.token);
-        this.$store.commit("setEmployeeData", res.data.employee);
+      let openid = this.$route.query.openid;
+      let appid = this.$route.query.appid;
+      if (openid != null && appid != null) {
+        let param = {
+          openid: openid,
+          appid: appid,
+        };
+        wxclientloginApi(param)
+          .then((res) => {
+            if (res.ack == "200") {
+              this.$store.commit("setAuthToken", res.data.token);
+              this.$store.commit("setEmployeeData", res.data.employee);
+              this.$router.push({ path: "/index" });
+            } else {
+              this.$router.push({
+                path: "/errorPage",
+                query: { msg: "没有权限", code: "901" },
+              });
+            }
+          })
+          .catch(() => {
+            this.$router.push({
+              path: "/errorPage",
+              query: { msg: "内部错误", code: "500" },
+            });
+          });
+        //alert(1);
+      } else {
+        loginApi()
+          .then((res) => {
+            if (res.ack == "200") {
+              this.$store.commit("setAuthToken", res.data.token);
+              this.$store.commit("setEmployeeData", res.data.employee);
+              this.$router.push({ path: "/index" });
+            } else {
+              this.$router.push({
+                path: "/errorPage",
+                query: { msg: "没有权限", code: "901" },
+              });
+            }
+          })
+          .catch(() => {
+            this.$router.push({
+              path: "/errorPage",
+              query: { msg: "内部错误", code: "500" },
+            });
+          });
       }
+      // let res = await loginApi();
+      // if (res.ack == "200") {
+      //   this.$store.commit("setAuthToken", res.data.token);
+      //   this.$store.commit("setEmployeeData", res.data.employee);
+      // }
     },
   },
 };
