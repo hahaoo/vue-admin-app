50cbd353af71f0b9602b35b302cc07f4325adbd9
Hunk <364125707@qq.com>
2021-03-05 14:09:25 +0800

    添加错误页面路由

--- src/router/index.js
+++ src/router/index.js
@@ -158,7 +158,6 @@ router.beforeEach((to, from, next) => {
     if (store.state.token) {
       next();
     } else {
-      
       // loginApi()
       //   .then((res) => {
       //     if (res.ack == "200") {
