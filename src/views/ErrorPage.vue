45afa014b8ea7dc435444a2891811ded28559749
Hunk <364125707@qq.com>
2021-03-05 13:33:42 +0800

    添加错误页面

---
+++ src/views/ErrorPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div style="font-size: 60px; font-weight: bold; text-align: center;color: #CCCCCC; margin-top: calc(40vh);">
+    	{{this.$route.query.code}}
+    </div>
+    <div style="text-align: center; font-size: 30px; color: #CCCCCC;">
+    	{{this.$route.query.msg}}
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
